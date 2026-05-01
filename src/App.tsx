27bec638a0df3b45b862a7dc940a803cd13ae38d
redoneabouaali-lab <redoneabouaali@gmail.com>
2026-05-01 18:12:12 +0100

    Upload src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChickenWebsite from './components/ChickenWebsite';
+
+function App() {
+  return (
+    <div className="App">
+      <ChickenWebsite />
+    </div>
+  );
+}
+
+export default App;
